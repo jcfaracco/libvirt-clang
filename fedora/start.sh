28474a9ce0f25bb2d5c664e997cbcc6801eb4f60
Julio Faracco <jcfaracco@gmail.com>
2019-04-02 10:40:01 -0300

    Adding extra steps to compile libvirt too.
    Signed-off-by: Julio Faracco <jcfaracco@gmail.com>

--- fedora/start.sh
+++ fedora/start.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 
 # Run libvirt docker with bindings.
-docker run -ti -v /tmp/:/tmp/:Z -p 8181:8181/tcp fedora:libvirt /bin/bash
+docker run -ti -v /tmp/:/tmp/:Z -p 8181:8181 fedora:libvirt /bin/bash
